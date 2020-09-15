78e817257f05f76fe6377fbf87fb0dd5ed5a4115
Ethan McGee <bulletshot60@users.noreply.github.com>
2020-09-15 08:57:47 -0400

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('todo/', include('todo.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django_prometheus.urls')),
 ]
