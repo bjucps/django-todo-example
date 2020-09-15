f9320104fca5deb3d2dc2d8b97fe2a74e937b9f9
Ethan McGee <bulletshot60@users.noreply.github.com>
2020-09-15 08:57:24 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
